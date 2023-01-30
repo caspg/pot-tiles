eb52fbe412d4751c2f3c248d915b23089f54e43e
Kacper Golinski <kacper.golinski@gmail.com>
2023-01-30 21:25:14 +0100

    use detached when running tileserver

--- run_tileserver.sh
+++ run_tileserver.sh
@@ -4,10 +4,8 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-# TOOD(kacper): change to detached mode
 # -d detached mode
-# docker run -d --restart unless-stopped \
-docker run --restart unless-stopped \
+docker run -d --restart unless-stopped \
   -v "$(pwd)/data":/data \
   -p 127.0.0.1:8080:8080 \
   maptiler/tileserver-gl:v4.4.3 -p 8080
